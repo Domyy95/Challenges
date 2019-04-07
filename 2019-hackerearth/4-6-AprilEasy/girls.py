004ff2dc0c487ee85d9f8f69dd9b1e0b395b1575
Unknown <domydome@hotmail.it>
2019-04-07 14:08:58 +0000

    Hackerearth April Easy challenge

---
+++ 2019-hackerearth/4-6-AprilEasy/girls.py
@@ -0,0 +1,41 @@
+import sys 
+
+def scrivi(result):
+	for i in range(0, len(result)):
+		print("Case #{}: {} ".format(i+1, result[i] ))
+
+if __name__ == '__main__':
+	
+	line1 = input()
+	N,K,P,Q = line1.split(" ") 
+	
+	L = float(P) / float(Q)
+
+	line2 = input()
+	a = list(line2.split(" "))
+
+	a.sort()
+
+	max = 0
+	for i in range(0,len(a)):
+		t = True
+		j = i + 1
+		while t and j<len(a):
+			if(float (a[j]) / int(a[i]) > L or j == len(a)-1):
+				if ( int(j-i) > max):
+					max = j-i
+				t = False
+			j = j+1
+	
+	max = max + int(K)
+	if (max > int (N)):
+		print(N)
+	else:
+		print(max)
+
+
+
+
+    
+
+    
---
+++ 2019-hackerearth/4-6-AprilEasy/string.py
@@ -0,0 +1,39 @@
+NO_OF_CHARS = 256
+
+def longestUniqueSubsttr(string): 
+    n = len(string) 
+    cur_len = 1        # To store the lenght of current substring 
+    max_len = 1        # To store the result 
+    prev_index = 0    # To store the previous index 
+    i = 0
+
+    visited = [-1] * NO_OF_CHARS 
+ 
+    visited[ord(string[0])] = 0
+
+    for i in range(1,n): 
+        prev_index = visited[ord(string[i])] 
+        if prev_index == -1 or (i - cur_len > prev_index): 
+            cur_len+=1
+    
+        else:  
+            if cur_len > max_len: 
+                max_len = cur_len 
+
+            cur_len = i - prev_index 
+
+        # update the index of current character 
+        visited[ord(string[i])] = i 
+
+    # Compare the length of last NRCS with max_len and update 
+    # max_len if needed 
+    if cur_len > max_len: 
+        max_len = cur_len 
+
+    return max_len 
+
+
+if __name__ == '__main__':
+    
+    s = input()
+    print(longestUniqueSubsttr(s))
