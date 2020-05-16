a21aff966244b252cb9354fcc6dffad04071439f
Dominic <domydome@hotmail.it>
2020-05-16 19:16:00 +0200

    Hackerearth May circuit first 2 problems

---
+++ 2020-Hackerearth/5 - May circuit/2_Factorial_equations.py
@@ -0,0 +1,18 @@
+# Problem Statement: https://www.hackerearth.com/challenges/competitive/may-circuits-20/algorithm/powerful-of-factorial-cb263e5b/
+'''
+You are given two numbers X and N. Your task is to find the last digit of the following equation:
+X ** ((N!)%10) 
+'''
+
+
+X,N = map(int, input().split())
+
+if N == 2:
+    print(X**2 % 10)
+elif N == 3:
+    print(X**6 % 10)
+elif N == 4:
+    print(X**4 % 10)
+
+else:
+    print(1)
---
+++ 2020-Hackerearth/5 - May circuit/README.md
@@ -0,0 +1,4 @@
+***May Circuits '20***
+
+[Link](https://www.hackerearth.com/challenges/competitive/may-circuits-20/problems/)
+
