2fa09dbdb760c7b3856e02068894eb3d2e5d889b
Dominic <domydome@hotmail.it>
2020-05-16 19:16:00 +0200

    Hackerearth May circuit first 2 problems

---
+++ 2020-Hackerearth/5 - May circuit/1_n_of_cycle.py
@@ -0,0 +1,13 @@
+# Problem Statement: https://www.hackerearth.com/challenges/competitive/may-circuits-20/algorithm/cycle-count-0644f309/
+'''
+You are given an N-sided regular polygon. You have connected the center of the polygon with all the vertices, thus dividing the polygon into N equal parts.
+
+Your task is to find the count of simple cycles that exist in the modified structure of the polygon.
+'''
+
+Q = int(input())                  
+
+for i in range(0,Q):
+    N = int(input())
+    sol = (N * (N-1)) +1
+    print(sol)
---
+++ 2020-Hackerearth/5 - May circuit/3_Serve_all_customers.py
@@ -0,0 +1,5 @@
+# Problem Statement: https://www.hackerearth.com/challenges/competitive/may-circuits-20/approximate/gotta-serveem-all-deb7cc6d/
+'''
+
+'''
+
