8189a4b1e6c9ddb116246cbbfa202e2ddcef4e23
Dominic <domydome@hotmail.it>
2020-05-25 17:26:10 +0200

    Hackerearth May circuit Third problem

--- 2020-Hackerearth/5 - May circuit/3_Serve_all_customers.py
+++ 2020-Hackerearth/5 - May circuit/3_Serve_all_customers.py
@@ -13,32 +13,56 @@ Note: Only one chef works to prepare an order of one person. A chef can only pre
 be prepared by one chef. Also, if a chef completes preparation of order at time , then he or she can pick up another order and start 
 preparing it at time t+1.
 '''
-N,K = map(int, input().split())
-A = list(map(int, input().split()))
-B = list(map(int, input().split()))
-C = list(map(int, input().split()))
+from heapq import heappush, heappop
+ 
+class Order():
+    __slots__ = ['index', 'arrival', 'preparation', 'cost']    
+    def __init__(self, index, arrival, preparation, cost):
+        self.index = index
+        self.arrival = arrival
+        self.preparation = preparation
+        self.cost = cost
 
-solution = []
-next_time_free = [A[0] for i in range(0,K)]
+    def __lt__(self, other):
+        # compare 2 orders: self < other <=> self must be served before other
+        # cost1: cost of serving self first, then serving other
+        cost1self = self.preparation * self.cost
+        cost1other = (max(self.arrival + self.preparation, other.arrival) - other.arrival) * other.cost + other.preparation * other.cost
+        cost1 = cost1self + cost1other
+        # cost2: cost of serving other first, then serving self
+        cost2other = other.preparation * other.cost
+        cost2self = (max(other.arrival + other.preparation, self.arrival) - self.arrival) * self.cost + self.preparation * self.cost
+        cost2 = cost2self + cost2other
+        return cost1 < cost2
+ 
+def solve(orders, N, K):
+    output = [0] * N
+    chefs = []
+    # all K chefs are immediately available at T = 0
+    for chef in range(K):
+        heappush(chefs, 0)
+    # process all orders in their sorted order
+    for order in orders:
+        # next chef is available at time T
+        T = heappop(chefs)
+        # and starts working at max(T, order.arrival)
+        T = max(T, order.arrival)
+        output[order.index] = T
+        # chef will be available at max(T, order.arrival) + order.preparation
+        heappush(chefs, T + order.preparation)
+    return ' '.join([str(x) for x in output])
+ 
+if __name__ == "__main__":
+    N, K = list(map(int, input().strip().split()))
+    A = list(map(int, input().strip().split()))  # customer arrival time
+    B = list(map(int, input().strip().split()))  # preparation time
+    C = list(map(int, input().strip().split()))  # cost per unit of time
+    orders = []
 
-for i,a in enumerate(A): 
-
-    chef = min(next_time_free)
-
-    if chef > 10**9:
-        break
-    
-    if A[i] <= chef:
-        solution.append(chef)
-        chef_new = chef + B[i]
-        next_time_free.remove(chef)
-        next_time_free.append(chef_new)
-    
-    else: 
-        solution.append(A[i])
-        chef_new = chef + B[i] + A[i]
-        next_time_free.remove(chef)
-        next_time_free.append(chef_new)
-    
-
-print(' '.join(str(e) for e in solution))
+    for index, (a, b, c) in enumerate(zip(A, B, C)):
+        orders.append(Order(index, a, b, c))
+        
+    # sort orders in the order in which they will be processed (the 1st chef
+    # available will process the next order in the line of orders)
+    orders = sorted(orders)
+    print(solve(orders, N, K))
