02f97f5abd5dd7b60d2eaf79afa9078ec9cf07a2
Dominic Crippa <dominic.crippa@gmail.com>
2022-03-10 20:40:24 +0100

    [ADD] - Reply coding challenge 2022

---
+++ 2022-3-10-ReplyCodeChallenge/main.py
@@ -0,0 +1,11 @@
+import sys
+
+from solver import Solver
+
+if __name__ == '__main__':
+    file_names = sys.argv[1].split("-")
+    for file_name in file_names:
+        print(f"Solving file {file_name}...")
+        problem = Solver(file_name)
+        problem.solve()
+        problem.to_output()
